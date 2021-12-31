7c285da845d8473510e8720ce7470edf7a24a24b
mattvargas <vargassormm1@montclair.edu>
2021-12-30 21:54:07 -0500

    weather form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import Form from "./Components/Form";
 
 function App() {
+  const [weather, setWeather] = useState({});
+
+  const getWeather = async () => {
+    const response = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?appid=4f0e1ee4f076d6cb61814091eae86568&zip=07036&units=imperial"
+    );
+    const data = await response.json();
+
+    setWeather(data);
+  };
+
+  console.log(weather);
+
   return (
     <div className="App">
       <h1>Weather Forecast</h1>
-      <Form />
+      <button onClick={getWeather}>Weather</button>
+      <p>{weather.main.temp}</p>
     </div>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 const Form = (props) => {
+  const [zip, setZip] = useState("");
+
   return (
     <form>
-      <input type="text" placeholder="Zip Code" />
+      <input
+        type="text"
+        placeholder="Zip Code"
+        value={zip}
+        onChange={(event) => setZip(event.target.value)}
+      />
       <button>Search</button>
     </form>
   );
